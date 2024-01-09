d488399e17ff36519b679e8033fe628cc4a62276
Xander-Junior <xanderbyiadom278@gmail.com>
2024-01-08 21:20:55 -0800

    Task 0: Create function to print list of integers

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,8 +6,8 @@ def main():
     names = dir(hidden_4)
     for name in sorted(names):
         if not name.startswith("__"):
-          
             print(name)
+
 
 if __name__ == "__main__":
     main()
