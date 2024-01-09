a0998d3063513a5a0e2f5727b4ff6f818ef7fc7f
Xander-Junior <xanderbyiadom278@gmail.com>
2024-01-08 21:49:38 -0800

    Task 8: Create function for multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence:
+        return len(sentence), sentence[0]
+    else:
+        return len(sentence), None
+
+
+if __name__ == "__main__":
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
