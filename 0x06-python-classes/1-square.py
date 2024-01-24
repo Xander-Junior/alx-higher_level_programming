378315f26814d1e4c931a4a18bf5b0c1dc10d93d
Xander-Junior <xanderbyiadom278@gmail.com>
2024-01-23 16:36:41 -0800

    Fixing issues

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-# 0-square.py by Obed Boakye
-"""Write a class Square that defines a square by: (based on 0-square.py) """
-
+# 1-square.py by Obed Boakye
+"""Module for Square class. This module provides a simple Square class with 
+initialization based on size."""
 
 class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
